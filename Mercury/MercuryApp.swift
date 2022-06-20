eccf24622feda329f02e65bb3c41912e7b15d86c
Daniel Correia <danielcorreia1988@outlook.com>
2022-06-20 17:43:10 +0200

    Initial Commit

---
+++ Mercury/MercuryApp.swift
@@ -0,0 +1,20 @@
+//
+//  MercuryApp.swift
+//  Mercury
+//
+//  Created by Daniel Correia on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MercuryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
